b572297207e97a51a50ada9bc9d20c4656a9f863
Yugyeong <uugyeong27@gmail.com>
2024-06-11 03:03:10 +0900

    submit1

--- priority_queue/19638.cpp
+++ priority_queue/19638.cpp
@@ -1,26 +1,67 @@
 /*
  * *** logic ***
- * 1.
+ * 1. 뿅망치 맞으면 h/2가 됨
+ * 2. h==1, 뿅망치 영향x
  *
  * *** Input ***
- * 1.
+ * 1. 인구수 N, 센티의 키 H, 뿅망치 횟수 T
+ * 2. 각 거인의 키 H
  *
  * *** Output ***
- * 1.
+ * 1. all H < Hcenti, print yes and 최소 뿅망치 횟수
+ * 2. T<0, print no and taller one
  *
  * *** Constraint ***
- * 1.
+ * 1. integer range - 1 ≤ N, T ≤ 10^5, 1 ≤ Hcenti, H ≤ 2 * 10^9
+ * 2. T를 최소한으로 사용하기
+ * 3. Hcenti == H -> No로 출력
+ * 4. 나머지는 버리기
+ *
+ * *** Solution - using pq  ***
+ * 1. descending sort
+ *
+ * *** submit1 - 바로 틀렸습니다 나옴 ***
  *
  */
 
 #include <iostream>
+#include <queue>
 using namespace std;
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
-    cout << "hello" << endl;
+    int N, Hcenti, T;
+    cin >> N >> Hcenti >> T;
 
-    return 0;
+    priority_queue <int, vector<int>, less<int>> pq;
+    int cnt=0;
+
+    for (int i=0;i<N;i++) {
+        int H;
+        cin >> H;
+        while (cnt<T) {
+            if (H==1) break;
+            if (H < Hcenti) break;
+            if (H >= Hcenti) H = H/2; cnt++;
+        }
+        pq.push(H);
+    }
+
+    for (int i=0;i<N;i++){
+        if (Hcenti <= pq.top()){
+            cout << "NO" << '\n';
+            cout << pq.top() << '\n';
+            return 0;
+        } else {
+            pq.pop();
+        }
+    }
+
+    if (pq.empty()) {
+        cout << "YES" << '\n';
+        cout << cnt << '\n';
+    }
+
 }
