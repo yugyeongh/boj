29d393d6b57170ee41640d8b2acab248950a5d87
Yugyeong <uugyeong27@gmail.com>
2024-06-11 01:37:57 +0900

    initial commit

---
+++ priority_queue/19638.cpp
@@ -0,0 +1,26 @@
+/*
+ * *** logic ***
+ * 1.
+ *
+ * *** Input ***
+ * 1.
+ *
+ * *** Output ***
+ * 1.
+ *
+ * *** Constraint ***
+ * 1.
+ *
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cout << "hello" << endl;
+
+    return 0;
+}
