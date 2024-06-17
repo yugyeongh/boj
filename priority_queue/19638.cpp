329270cb94ef594c7ead77191484b3ba12069f3c
Yugyeong <uugyeong27@gmail.com>
2024-06-17 16:24:38 +0900

    맞왜틀

--- priority_queue/19638.cpp
+++ priority_queue/19638.cpp
@@ -21,10 +21,12 @@
  * 1. descending sort
  *
  * *** submit1 - 바로 틀렸습니다 나옴 ***
- *
+ * 1. 테케는 다 통과인데 왜 틀릴까
+ * 2. 진정한 맞왜틀
  */
 
 #include <iostream>
+#include <vector>
 #include <queue>
 using namespace std;
 
@@ -41,18 +43,29 @@ int main() {
     for (int i=0;i<N;i++) {
         int H;
         cin >> H;
-        while (cnt<T) {
-            if (H==1) break;
-            if (H < Hcenti) break;
-            if (H >= Hcenti) H = H/2; cnt++;
-        }
+//        while (cnt<T) {
+//            if (H==1) break;
+//            if (H < Hcenti) break;
+//            if (H >= Hcenti) H = H/2; cnt++;
+//        }
         pq.push(H);
     }
+
+    for (int i=0;i<T;i++) {
+        int h = pq.top();
+
+        while (cnt<T) {
+            if (h==1) break;
+            if (h < Hcenti) break;
+            if (h >= Hcenti) h = h/2; cnt++;
+        }
+    }
+
 
     for (int i=0;i<N;i++){
         if (Hcenti <= pq.top()){
             cout << "NO" << '\n';
-            cout << pq.top() << '\n';
+            cout << pq.top();
             return 0;
         } else {
             pq.pop();
@@ -61,7 +74,8 @@ int main() {
 
     if (pq.empty()) {
         cout << "YES" << '\n';
-        cout << cnt << '\n';
+        cout << cnt;
+        return 0;
     }
 
 }
