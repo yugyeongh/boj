58f843493336b7f92a70c817b4fc8c51600996dc
Yugyeong <uugyeong27@gmail.com>
2024-06-27 10:40:49 +0900

    reset

--- bfs_dfs/16946.cpp
+++ bfs_dfs/16946.cpp
@@ -1,72 +1,9 @@
 #include <iostream>
-#include <queue>
-#include <cstdio>//scanf
-#include <cstring>//memset
 using namespace std;
 
-struct Pos {
-    int x, y;
-};
-
-int N, M;
-int Map[1001][1001];
-bool visited[1001][1001];
-int dx[] = { -1,1,0,0 };
-int dy[] = { 0,0,-1,1 };
-
-void bfs(int startx, int starty) {
-    queue<Pos>q;
-    vector<Pos>wall;
-    q.push({ startx,starty });
-    visited[startx][starty] = true;
-    int cnt = 1;
-    while (!q.empty()) {
-        int x = q.front().x;
-        int y = q.front().y;
-        q.pop();
-        for (int i = 0; i < 4; i++) {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-
-            if (nx < 0 || nx >= N || ny < 0 || ny >= M) continue;
-
-            if (Map[nx][ny] == 0 && visited[nx][ny] == false) {
-                visited[nx][ny] = true;
-                q.push({ nx,ny });
-                cnt++;
-            }
-
-            else if (Map[nx][ny] != 0 && visited[nx][ny] == false) { //현재 탐색하는 공간에 인접한 벽 저장
-                visited[nx][ny] = true;
-                wall.push_back({ nx,ny });
-            }
-        }
-    }
-
-    for (int i = 0; i < wall.size(); i++) { //탐색하면서 나왔던 벽들에 0의 개수 더해주기
-        Map[wall[i].x][wall[i].y] += cnt;
-        visited[wall[i].x][wall[i].y] = false;
-    }
-}
-
 int main() {
-    scanf("%d %d", &N, &M);
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < M; j++) {
-            scanf("%1d", &Map[i][j]);
-        }
-    }
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < M; j++) {
-            if (Map[i][j] == 0 && visited[i][j] == false)
-                bfs(i, j);
-        }
-    }
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < M; j++) {
-            printf("%d", Map[i][j]%10);
-        }
-        printf("\n");
-    }
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
     return 0;
 }
