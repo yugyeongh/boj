fc9348c6867d62e87452f9433c891bb8d09de218
Yugyeong <uugyeong27@gmail.com>
2024-06-30 18:07:04 +0900

    다시 풀어야지

--- bfs_dfs/7569.cpp
+++ bfs_dfs/7569.cpp
@@ -1,82 +1,75 @@
 // 3차원 자료구조 사용하기
 // 흑흑
+// 인덱스가 제일 헷갈린다🫥
 
 #include <iostream>
 #include <queue>
 #include <vector>
 using namespace std;
 
-int N,M,H;
-int tomato;
-vector <vector <vector<int>>> box;
-bool visited[101][101][101]={false,};
-queue <pair<int, pair<int,int>>> q;
+int M,N,H; // 가로, 세로, 높이
+int box[101][101][101];
+int day[101][101][101]={-1,};
+queue <pair<pair<int,int>, int>> q;
 
 int dx[6]={-1,1,0,0,0,0};
 int dy[6]={0,0,0,0,-1,1};
 int dz[6]={0,0,-1,1,0,0};
 
-void bfs(int c, int a, int b){
-    q.push({c,{a,b}});
-    visited[c][a][b]=true;
-
-    while (!q.empty()){
-        int z = q.front().first;
-        int x = q.front().second.first;
-        int y = q.front().second.second;
-        q.pop();
-
-        for (int i=0;i<6;i++){
-            int nz = z + dz[i];
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-
-            if (nx<0 || ny<0 || nz<0 || nx>=N || ny>=M || nz>=H) continue;
-            if (box[nz][nx][ny]==0){
-                q.push({nz,{nx,ny}});
-                box[nz][nx][ny]= box[z][x][y]+1;
-            }
-        }
-    }
-}
-
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
-    cin >> N >> M >> H;
-    for (int k=0;k<H;k++){
-        for (int i=0;i<N;i++){
-            for (int j=0;j<M;j++){
-                cin >> tomato;
-                box[k][i][j]=tomato;
-            }
-        }
-    }
+    cin >> M >> N >> H; // 가로, 세로, 높이
 
     for (int k=0;k<H;k++){
         for (int i=0;i<N;i++){
             for (int j=0;j<M;j++){
-                if (box[k][i][j]==1){
-                    bfs(k,i,j);
+                cin >> box[i][j][k];
+                day[i][j][k]=-1;
+                if (box[i][j][k]==1){
+                    q.push({{i,j},k});
+                    day[i][j][k]=0;
                 }
             }
         }
     }
 
-    int max=0;
-    for (int k=0;k<H;k++){
-        for (int i=0;i<N;i++){
-            for (int j=0;j<M;j++){
-                if (box[k][i][j]==1 && box[k][i][j]>max){
-                    max = box[k][i][j];
-                } else {
-                    cout << "-1";
-                    return 0;
-                }
+    while (!q.empty()){
+        int x = q.front().first.first;
+        int y = q.front().first.second;
+        int z = q.front().second;
+        q.pop();
+
+        for (int i=0;i<6;i++){
+            int nx = x+dx[i];
+            int ny = y+dy[i];
+            int nz = z+dz[i];
+
+            if (nx<0 || ny <0 || nz<0 || nx>=N || ny>=M || nz>=H) continue;
+            if (box[nx][ny][nz]==0 && day[nx][ny][nz]==-1){ // 안 익은 토마토 && 방문 x
+                q.push({{nx,ny},nz});
+                day[nx][ny][nz] = day[x][y][z]+1;
             }
         }
     }
+
+    int ans=0;
+    for (int i=0;i<N;i++){
+        for (int j=0;j<M;j++){
+            for (int k=0;k<H;k++){
+                if (box[i][j][k]==0 && day[i][j][k]==-1){ // 안 익은 토마토 && 방문x
+                    ans=-1;
+                    break;
+                }
+                if (ans<day[i][j][k]) ans=day[i][j][k];
+            }
+            if (ans==-1) break;
+        }
+        if (ans==-1) break;
+    }
+
+    cout << ans;
 
     return 0;
 }
