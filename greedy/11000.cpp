b7ea5efaada7caefe8294104ce4d6e74a3cc95ac
Yugyeong <uugyeong27@gmail.com>
2024-07-04 23:07:26 +0900

    complete using vector, pq

--- greedy/11000.cpp
+++ greedy/11000.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
+#include <vector>
 #include <queue>
+#include <algorithm>
 using namespace std;
 
 int N;
-priority_queue <pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
+vector <pair<int,int>> v;
+priority_queue <int , vector<int>, greater<int>> pq;
 
 int main() {
     ios::sync_with_stdio(false);
@@ -14,22 +17,20 @@ int main() {
     cin >> N;
     for (int i=0;i<N;i++){
         cin >> S >> T;
-        pq.push({S,T});
+        v.push_back({S,T});
     }
+    sort(v.begin(), v.end());
 
-
-    int cnt=1;
-    int end = pq.top().second;
-    pq.pop();
-    while (!pq.empty()){
-        if (end <= pq.top().first){
-            cnt++;
-            end = pq.top().second;
+    pq.push(v[0].second);
+    for (int i=1;i<N;i++){
+        if (pq.top() <= v[i].first){
+            pq.pop();
+            pq.push(v[i].second);
+        } else {
+            pq.push(v[i].second);
         }
-        pq.pop();
     }
 
-    cout << cnt << '\n';
-
+    cout << pq.size() << '\n';
     return 0;
 }
