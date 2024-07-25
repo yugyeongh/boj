615b03018e29df8c1c56a0db2846d0076cbbf56d
Yugyeong <uugyeong27@gmail.com>
2024-07-25 16:23:58 +0900

    [S4] 좋은 단어

--- data_structure/3986.cpp
+++ data_structure/3986.cpp
@@ -1,14 +1,32 @@
-/*
- * *** solution ***
- *
- */
-
 #include <iostream>
+#include <stack>
 using namespace std;
+
+int N;
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
+    cin >> N;
+
+    int ans=0;
+    while (N--){
+        stack <char> s;
+        string word;
+        cin >> word;
+
+        for (int i=0;i<word.length();i++){
+            if (s.empty()){
+                s.push(word[i]);
+            } else {
+                if (s.top() == word[i]) s.pop();
+                else s.push(word[i]);
+            }
+        }
+
+        if (s.empty()) ans++;
+    }
+    cout << ans;
     return 0;
 }
