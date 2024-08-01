bb1f65cfc6bf3551668d83de9f8327213300a029
Yugyeong <uugyeong27@gmail.com>
2024-08-01 16:42:08 +0900

    [G4] 오큰수

--- searching/17298.cpp
+++ searching/17298.cpp
@@ -1,37 +1,47 @@
 #include <iostream>
-#include <queue>
+#include <vector>
+#include <stack>
+#define MAX 1000001
 using namespace std;
 
-int N, temp;
+int N;
+int arr[MAX]={0,};
+int nge[MAX]={0,};
+stack <int> s;
+
+// 스택에 뭘 넣을지 1. 자기보다 큰 수를 넣을지 2. 큰 수의 인덱스를 넣을지 고민해야 하는데
+// 보통 인덱스를 넣음
+// 지기보다 큰 수가 없을 때까지 작은 수를 다 빼고 큰 수만 바라보게 함
+// 앞서 들어온 수가 작으면 뒤에 있는 수를 삭제해야함
+// 쏙속 이해됨
+// 6549 비슷한 문제임
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
     cin >> N;
-    priority_queue <pair<int,int>, vector<pair<int,int>>, less<pair<int,int>>> pq;
+
+    for (int i=0;i<N;i++) {
+        cin >> arr[i];
+    }
+
+    for (int i=0;i<N;i++) {
+        while (!s.empty() && arr[s.top()]<arr[i]) {
+            nge[s.top()] = arr[i];
+            s.pop();
+        }
+        s.push(i);
+    }
+
+    while (!s.empty()) {
+        nge[s.top()] = -1;
+        s.pop();
+    }
 
     for (int i=0;i<N;i++){
-        cin >> temp;
-        pq.push({i,temp});
+        cout << nge[i] << " ";
     }
-
-    int idx = pq.top().first;
-    int a = pq.top().second;
-    pq.pop();
-    while (!pq.empty()){
-        if (a < pq.top().second){
-            cout << pq.top().second << " ";
-            a = pq.top().second;
-            break;
-        } else {
-            cout << "-1 ";
-        }
-    }
-
-
-
-    cout << '\n';
 
     return 0;
 }
