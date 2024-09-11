497b9cdae06f3937a1d1f23f68672f5b80718410
Yugyeong <uugyeong27@gmail.com>
2024-09-12 08:34:54 +0900

    풀이1 wrong

---
+++ 1806.cpp
@@ -0,0 +1,47 @@
+/*
+ * 입력: 수열의 길이 N, 부분합 S, 수열
+ * 출력: S의 최소 길이 (불가능하다면 0 출력)
+ *
+ * 풀이1
+ * 1. 어떤 자료구조를 이용할까? - 벡터 (bc, 부분합의 시작 인덱스를 기억하고 불가능하면 바로 현재부터 다시 인덱스를 기억하면 됨)
+ * 2. v[i] 자체를 넣지 않고 v[i]=v[i]+v[i-1]을 저장할까? 5 6 9 14 24 31 35 44 46 54
+ * 3. v[start]-v[end]가 (1) <S면 end++, (2) > S면 start++, (3) 같으면 end-start 출력
+ */
+
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int N, S;
+    cin >> N >> S;
+
+    int temp;
+    vector <int> v(N);
+    cin >> v[0];
+    for (int i=1;i<N;i++){
+        cin >> temp;
+        v[i] = v[i-1]+temp;
+    }
+
+    int start=0, end=0;
+    while (end != N){
+        if (v[end]-v[start] < S){
+            end++;
+        } else if (v[end]-v[start] > S){
+            start++;
+        } else if (v[end]-v[start] == S){
+            cout << end-start << '\n';
+            return 0;
+        }
+    }
+
+    cout << 0 << '\n';
+
+    return 0;
+}
