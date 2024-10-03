36211273806553312d016e038747a1fd9a393b13
Yugyeong <uugyeong27@gmail.com>
2024-10-03 14:21:48 +0900

    정답

--- 2473.cpp
+++ 2473.cpp
@@ -7,8 +7,10 @@
 // 출력: 특성값을 0에 가깝게 만드는 세 용액의 특성값 출력 (오름차순으로)
 //
 // 풀이
-// - 주어진 용액의 특성값을 정렬하고 투포인터를 사용해 구하자! -> 사용했었으나 잘 안풀려서 3중 for문 씀
+// - 주어진 용액의 특성값을 정렬하고 투포인터를 사용해 구하자!
 // - 3개의 용액이니 3개의 지점을 가리켜야함
+// - if sum<0, second를 오른쪽으로 옮겨 큰 수를 가리키도록 하고
+// - if sum>0, third를 왼쪽으로 옮겨 작은 수를 가리키도록 함
 // - 0에 가까운 합을 어떻게 찾아낼까 -> 절대값
 //
 
@@ -20,9 +22,8 @@
 using namespace std;
 
 int N;
-int zero=INF;
 vector <int> v;
-vector <int> answer;
+vector <int> answer(3);
 
 int main() {
     ios::sync_with_stdio(false);
@@ -37,20 +38,25 @@ int main() {
 
     sort(v.begin(), v.end());
 
+    long long zero=3000000000L;
+    long long sum=0;
+    for (int first=0;first<N-2;first++){
+        int second = first+1;
+        int third = N-1;
 
-    for (int i=0;i<N;i++){
-        for (int j=i+1;j<N;j++){
-            for (int k=j+1;k<N;k++){
-                int sum = v[i] + v[j] + v[k];
+        while (second < third){
+            sum = (long long)v[first] + (long long)v[second] + (long long)v[third];
 
-                if (abs(sum) <= abs(zero)){
-                    answer.clear();
-                    answer.push_back(v[i]);
-                    answer.push_back(v[j]);
-                    answer.push_back(v[k]);
-                    zero = sum;
-                }
+            if (abs(sum) < zero){
+                zero = abs(sum);
+                answer.clear();
+                answer.push_back(v[first]);
+                answer.push_back(v[second]);
+                answer.push_back(v[third]);
             }
+
+            if (sum < 0) second++;
+            else third--;
         }
     }
 
